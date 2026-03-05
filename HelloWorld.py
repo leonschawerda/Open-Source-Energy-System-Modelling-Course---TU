70ce3986dfa2b780579a192e823747308e49e6b2
Leon Schawerda <e11709119@student.tuwien.ac.at>
2026-03-05 10:16:22 +0100

    added some info regarding git commands

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,6 +4,10 @@ Goal:
 Clone the git repository
 add Hello World to repository
 
+git clone [URL]: Get Git Repository initially
+git add [File]: adds file that should be uploaded to the git repository
+git commit -m'' : Commits all added changes including the Message to the git repository
+git push: Pushes all commited changes to the online Git Repository
 '''
 # Print Hello World 
 print("Hello World")
