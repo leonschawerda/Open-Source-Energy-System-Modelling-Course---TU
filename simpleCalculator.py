8b10abd23de66f58e8374140bf4273d88fb3d577
Leon Schawerda <e11709119@student.tuwien.ac.at>
2026-03-16 14:53:34 +0100

    added multiply and divide function

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,10 +1,17 @@
 def add(num1, num2, num3=0):
     return num1 + num2 + num3
 
+def subtract (num1, num2):
+    return (num1-num2)
+
+def multiply (x,y):
+    return (x*y)
+
+def divide (x,y):
+    return x/y
+
 # main
 
 print(add (1, 3, 4))
 
-def subtract (num1, num2):
-    return (num1-num2)
 
