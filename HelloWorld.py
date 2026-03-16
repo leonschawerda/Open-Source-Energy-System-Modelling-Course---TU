346ac7dc1a56845c8ad27eed923af8d21f9a54e0
Leon Schawerda <e11709119@student.tuwien.ac.at>
2026-03-16 14:09:17 +0100

    added a subtract function

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,5 +9,11 @@ git add [File]: adds file that should be uploaded to the git repository
 git commit -m'' : Commits all added changes including the Message to the git repository
 git push: Pushes all commited changes to the online Git Repository
 '''
+
+#some more comments
 # Print Hello World 
 print("Hello World")
+
+#added another comment
+#another line
+
--- simpleCalculator.py
+++ simpleCalculator.py
@@ -5,3 +5,6 @@ def add(num1, num2, num3):
 
 print(add (1, 3, 4))
 
+def subtract (num1, num2):
+    return (num1-num2)
+
