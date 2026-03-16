6869d988dc6f9960ea2b65766688ba770ea66d28
Leon Schawerda <e11709119@student.tuwien.ac.at>
2026-03-16 14:55:52 +0100

    added tests for multiply, divide, and substract

--- test_simpleCalculator.py
+++ test_simpleCalculator.py
@@ -2,5 +2,13 @@ import simpleCalculator
 
 
 def test_add():
-
 	assert simpleCalculator.add(2, 4) == 6
+	
+def test_substract():
+	assert simpleCalculator.subtract(6, 4) == 2
+	
+def test_multiply():
+	assert simpleCalculator.multiply(2, 4) == 8
+	
+def test_divide():
+	assert simpleCalculator.divide(8, 4) == 2
