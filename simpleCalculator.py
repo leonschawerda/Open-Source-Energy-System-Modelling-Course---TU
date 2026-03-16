0d50c6df2b7bdb0c8959b1f7164ee81e5b4af3a9
Leon Schawerda <e11709119@student.tuwien.ac.at>
2026-03-16 15:30:19 +0100

    removed unused import from simple calculator

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -13,13 +13,13 @@ def subtract (num1, num2):
     return (num1-num2)
 
 def multiply (x,y):
-    return (x*y)
+    return (x * y)
 
 def divide (x,y):
     return x/y
 
 # main
 
-print(add (1, 3, 4))
+print("hello")
 
 
